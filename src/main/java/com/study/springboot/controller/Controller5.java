b1fecaa771760e18a1bda389fca8c62fd6dbab72
easy-seong <lee89928@nate.com>
2023-02-07 15:41:16 +0900

    지성 업데이트
    컨트롤러5 최신화
    
    qna폴더에 html 파일 2개 추가
    (listForm & qnaText)

--- src/main/java/com/study/springboot/controller/Controller5.java
+++ src/main/java/com/study/springboot/controller/Controller5.java
@@ -1,4 +1,15 @@
 package com.study.springboot.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controller5 {
+    @GetMapping("templates/admin/qna/listForm")
+    public String main() {
+        return "templates/admin/qna/listForm";
+    }
+
+
 }
