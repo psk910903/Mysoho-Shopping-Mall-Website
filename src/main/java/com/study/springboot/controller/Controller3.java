734d1f8b0293ec786606d23a87adc923bd9b4dc0
ejkim230101 <mit230101@gmail.com>
2023-02-08 18:21:50 +0900

    이미지

--- src/main/java/com/study/springboot/controller/Controller3.java
+++ src/main/java/com/study/springboot/controller/Controller3.java
@@ -3,7 +3,7 @@ package com.study.springboot.controller;
 import com.study.springboot.dto.review.ReviewSaveDto;
 import com.study.springboot.entity.review.Review;
 import com.study.springboot.repository.ReviewRopository;
-import com.study.springboot.service.ReviewService;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,7 +18,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @RequestMapping("/review")
 public class Controller3 {
-    private final ReviewService reviewService;
+    //private final ReviewService reviewService;
     private final ReviewRopository reviewRopository;
     //url:http://localhost:8080/review/
     @RequestMapping("/")
@@ -27,6 +27,7 @@ public class Controller3 {
     }
     @RequestMapping("/listForm")
     public String list(Model model){
+        //정렬해서 검색하면 되는지 보기
         List <Review> list = reviewRopository.findAll();
         model.addAttribute("list",list);
         model.addAttribute("listcount",list.size());
@@ -54,9 +55,12 @@ public class Controller3 {
     }
 //    throws Exception
 //    return "/admin/review/review2";
-//    @RequestMapping("delete")
-//    public String delete(){
-//
-//    }
+    @RequestMapping("search")
+    public String search(){
+        //findByName
+        //findByMemberId
+        return "/admin/review/test";
+
+    }
 
 }
--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -171,16 +171,23 @@ SELECT * FROM order;
 -- 리뷰
 DROP TABLE if EXISTS review;
 CREATE TABLE review (
-	review_no INT AUTO_INCREMENT NOT NULL PRIMARY KEY, -- 고유키
-	member_id VARCHAR(255) NOT NULL, -- 아이디(회원) / 비회원은 후기를 달수없음
-  item_code VARCHAR(255) NOT NULL UNIQUE, -- 상품 코드(UUID포맷-32자리)
-	review_star TINYINT NOT NULL,  -- 별점
+   review_no INT AUTO_INCREMENT NOT NULL PRIMARY KEY, -- 고유키
+   member_id VARCHAR(255) NOT NULL, -- 아이디(회원) / 비회원은 후기를 달수없음
+  item_name VARCHAR(255) NOT NULL UNIQUE, -- 상품 코드(UUID포맷-32자리)
+   review_star TINYINT NOT NULL,  -- 별점
   review_content TEXT NULL,  -- 상품후기
-	review_image_url TEXT NULL, -- 이미지
-	review_datetime DATETIME DEFAULT NOW() -- 작성시간
+   review_image_url TEXT NULL, -- 이미지
+   review_datetime DATE DEFAULT NOW(), -- 작성시간
+   review_exposure VARCHAR(255) DEFAULT '표시함' -- 노출여부
 );
 INSERT INTO review
-	VALUES (NULL, 'hong', '123e4567-e89b-12d3-a456-556642440000', '5', '가성비 좋아요', NULL, default);
+   VALUES (NULL, 'hong', '린넨ops베이지', '5', '가성비 좋아요', NULL, DEFAULT,DEFAULT );
+INSERT INTO review
+   VALUES (NULL, 'tom', '도트sk오렌지', '4', '배송이 빨라요', NULL, DEFAULT,DEFAULT);
+INSERT INTO review
+   VALUES (NULL, 'hana', '골지원피스', '4.5', '옷이 예뻐요', NULL, DEFAULT,DEFAULT);
+INSERT INTO review
+   VALUES (NULL, 'hong', '린넨ops베이지2', '5', '가성비 좋아요', NULL, DEFAULT,DEFAULT );
 SELECT * FROM review;
 
 -- QNA 질문
