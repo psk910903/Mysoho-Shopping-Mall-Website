ed94d471304a93402b9f92413386d8ad7857f12f
psk910903 <psk910903@gmail.com>
2023-02-10 17:43:20 +0900

    0210 삭제

--- src/main/java/com/study/springboot/entity/BoardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.study.springboot.entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BoardRepository extends JpaRepository<Board, Long> {
-
-}
