e2522f1587c26df9951e0f69ef95ea0c73c9b5af
Seotriever <122240520+Seotriever@users.noreply.github.com>
2023-02-10 17:35:05 +0900

    0210 경빈

---
+++ src/main/java/com/study/springboot/dto/member/MemberResponseDto.java
@@ -0,0 +1,45 @@
+package com.study.springboot.dto.member;
+
+
+import com.study.springboot.entity.Board;
+import com.study.springboot.entity.Member.MemberEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class MemberResponseDto {
+    private Long member_no;
+    private String member_name;
+    private String member_id;
+
+    private String member_pw;
+    private String member_phone;
+    private String member_email;
+    private Long member_mileage;
+    private String member_address;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate member_join_datetime;
+
+
+
+
+    public MemberResponseDto(MemberEntity entity) {
+        this.member_no = entity.getMember_no();
+        this.member_name = entity.getMember_name();
+        this.member_id = entity.getMember_id();
+        this.member_pw = entity.getMember_pw();
+        this.member_phone = entity.getMember_phone();
+        this.member_email = entity.getMember_email();
+        this.member_join_datetime = entity.getMember_join_datetime();
+        this.member_mileage = entity.getMember_mileage();
+        this.member_address = entity.getMember_address();
+
+}
+}
