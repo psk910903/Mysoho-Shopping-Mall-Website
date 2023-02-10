8b15a6f06772ed2fe62c13b4b47c685522db4723
Seotriever <122240520+Seotriever@users.noreply.github.com>
2023-02-10 17:36:26 +0900

    0210 경빈

--- src/main/java/com/study/springboot/repository/MemberRepository.java
+++ src/main/java/com/study/springboot/repository/MemberRepository.java
@@ -1,6 +1,6 @@
 package com.study.springboot.repository;
 
-import com.study.springboot.entity.Member;
+import com.study.springboot.entity.Member.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,9 +9,11 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
 
     @Query(value = "select * from member m where m.user_id = :user_id_param and m.user_pw = :user_pw_param", nativeQuery = true)
-    List<Member> findByUserIdAndUserPw(@Param("user_id_param") String user_id,
-                                       @Param("user_pw_param") String user_pw);
+    List<MemberEntity> findByUserIdAndUserPw(@Param("user_id_param") String user_id,
+                                             @Param("user_pw_param") String user_pw);
+
+
 }
