dea9f6bb1980d45133af8cf3ee4e47a6922bef05
psk910903 <psk910903@gmail.com>
2023-02-10 18:14:34 +0900

    0210 repository

--- src/main/java/com/study/springboot/repository/CartRepository.java
+++ src/main/java/com/study/springboot/repository/CartRepository.java
@@ -3,10 +3,11 @@ package com.study.springboot.repository;
 import com.study.springboot.entity.CartEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CartRepository extends JpaRepository<CartEntity, Long> {
 
     //네이티브 쿼리
     @Query(value = "SELECT * FROM cart WHERE cart_code = :cart_code", nativeQuery = true)
-    CartEntity findByCartCodeNativeQuery(String cart_code);
+    CartEntity findByCartCodeNativeQuery(@Param("cart_code") String cart_code);
 }
