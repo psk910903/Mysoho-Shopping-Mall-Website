ef7cca672b79dc5c0aca90e0a50b8c8af1e4f093
psk910903 <104986080+psk910903@users.noreply.github.com>
2023-02-16 14:43:14 +0900

    0216 선교 aws 서비스 추가, 장바구니, 주문, 상품 서비스 통합

--- src/main/java/com/study/springboot/service/OrderService.java
+++ src/main/java/com/study/springboot/service/OrderService.java
@@ -1,7 +1,7 @@
 package com.study.springboot.service;
 
-import com.study.springboot.dto.order.OrderInfoSaveRequestDto;
-import com.study.springboot.dto.order.OrderListResponseDto;
+import com.study.springboot.dto.order.OrderContentSaveRequestDto;
+import com.study.springboot.dto.order.OrderResponseDto;
 import com.study.springboot.entity.*;
 import com.study.springboot.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,9 +12,14 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
@@ -23,66 +28,56 @@ public class OrderService {
 
     //리스트 페이징
     @Transactional(readOnly = true)
-    public Page<OrderEntity> getList(int page) {
-//        List<Sort.Order> sorts = new ArrayList<>();
-//        sorts.add(Sort.Order.desc("item_update_datetime")); //최신글을 먼저 보여준다.
-//        Pageable pageable = PageRequest.of(page, 5, Sort.by(sorts)); //5개씩
+    public Page<OrderResponseDto> findAll(int page) {
 
-        Pageable pageable = PageRequest.of(page, 5); //5개씩
-        return orderRepository.findAll(pageable);
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("orderDatetime"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        Page<OrderEntity> list = orderRepository.findAll(pageable);
+
+        return list.map(OrderResponseDto::new);
     }
+
     //페이징 5개로 고정
-    @Transactional(readOnly = true)
-    public List<Integer> getPageList(int page, Page<OrderEntity> paging) {
-        int totalPage = paging.getTotalPages();
+    public List<Integer> getPageList(final int totalPage, final int page) {
+
         List<Integer> pageList = new ArrayList<>();
+
         if (totalPage <= 5){
             for (Integer i=0; i<=totalPage-1; i++){
                 pageList.add(i);
             }
-            return pageList;
         }else if(page >= 0 && page <= 2){
             for (Integer i=0; i<=4; i++){
                 pageList.add(i);
             }
-            return pageList;
         }
         else if (page >= totalPage-3 && page <= totalPage-1){
             for (Integer i=5; i>=1; i--){
                 pageList.add(totalPage - i);
             }
-            return pageList;
         }else{
             for (Integer i=-2; i<=2; i++){
                 pageList.add(page + i);
             }
-            return pageList;
         }
-    }
-    //페이징에서 dto 리스트 꺼내기
-    @Transactional(readOnly = true)
-    public List<OrderListResponseDto> getDtoList(Page<OrderEntity> paging){
-        List<OrderListResponseDto> list = new ArrayList<>();
-        for (OrderEntity entity : paging) {
-            list.add(new OrderListResponseDto(entity));
-        }
-        return list;
+
+        return pageList;
     }
 
-
     @Transactional(readOnly = true)
-    public OrderListResponseDto findById(Long id) {
+    public OrderResponseDto findById(Long id) {
         OrderEntity entity = orderRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
-        return new OrderListResponseDto(entity);
+        return new OrderResponseDto(entity);
     }
 
     @Transactional
-    public boolean updateOrderInfo(OrderInfoSaveRequestDto dto) {
+    public boolean updateOrderContent(OrderContentSaveRequestDto dto) {
         try {
-            OrderEntity entity = orderRepository.findById(dto.getOrder_no()).get();
-            OrderListResponseDto responseDto = new OrderListResponseDto(entity);
-            dto.setOrder_datetime(responseDto.getOrder_datetime());
+            OrderEntity entity = orderRepository.findById(dto.getOrderNo()).get();
+            OrderResponseDto responseDto = new OrderResponseDto(entity);
+            dto.setOrderDatetime(responseDto.getOrderDatetime());
             orderRepository.save(dto.toEntity());
         } catch (Exception e) {
             e.printStackTrace();
@@ -92,11 +87,11 @@ public class OrderService {
     }
 
     @Transactional
-    public boolean stateUpdate(Long id, String orderState) {
+    public boolean statusModify(Long id, String orderState) {
 
         try {
-            OrderListResponseDto dto = findById(id);
-            dto.setOrder_state(orderState);
+            OrderResponseDto dto = findById(id);
+            dto.setOrderState(orderState);
             orderRepository.save(dto.toEntity());
         } catch (Exception e) {
             e.printStackTrace();
@@ -104,4 +99,87 @@ public class OrderService {
         }
         return true;
     }
+
+    @Transactional(readOnly = true)
+    public Page<OrderResponseDto> findByKeyword(String findBy, String keyword, int page) {
+
+        Page<OrderEntity> list;
+        Pageable pageable = PageRequest.of(page, 10);
+
+        if (findBy.equals("all")) {
+            list = orderRepository.findByAllContaining(keyword, pageable);
+        }else if (findBy.equals("orderName")){
+            list = orderRepository.findByOrderNameContaining(keyword, pageable);
+        }else if (findBy.equals("orderPhone")) {
+            list = orderRepository.findByOrderPhoneContaining(keyword, pageable);
+        }else if (findBy.equals("orderNo")) {
+            list = orderRepository.findByOrderNoContaining(keyword, pageable);
+        } else {
+            //주문상태 검색
+            list = orderRepository.findByOrderState(keyword, pageable);
+        }
+
+        return list.map(OrderResponseDto::new);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<OrderResponseDto> findByDate(String start, String end, int page) throws ParseException {
+
+        Page<OrderEntity> list;
+        Pageable pageable = PageRequest.of(page, 10);
+
+        //문자열을 날짜형식으로 변환
+        DateFormat sdFormatStart = new SimpleDateFormat("yyyy-MM-dd");
+        DateFormat sdFormatEnd = new SimpleDateFormat("yyyy-MM-dd");
+        Date tempDateStart = sdFormatStart.parse(start);
+        Date tempDateEnd = sdFormatEnd.parse(end);
+        Calendar cal1 = Calendar.getInstance();
+        Calendar cal2 = Calendar.getInstance();
+        cal1.setTime(tempDateStart);
+        cal2.setTime(tempDateEnd);
+        DateFormat df1 = new SimpleDateFormat("yyyy-MM-dd");
+        DateFormat df2 = new SimpleDateFormat("yyyy-MM-dd");
+        cal2.add(Calendar.DATE, +1);
+
+        //날짜형식을 문자열로 변환
+        String dateStartStr = df1.format(cal1.getTime())+" 00:00:00";
+        String dateEndStr = df2.format(cal2.getTime())+" 00:00:00";
+
+        list = orderRepository.findByOrderNoContaining(dateStartStr, dateEndStr, pageable);
+        return list.map(OrderResponseDto::new);
+    }
+
+
+    public Page<OrderResponseDto> findByDate(String mode, int page) throws ParseException {
+
+        //오늘날짜로 date객체 2개 생성 (~부터 ~까지로 검색에 사용목적)
+        Date tempDateStart = new Date();
+        Date tempDateEnd = new Date();
+        Calendar cal1 = Calendar.getInstance();
+        Calendar cal2 = Calendar.getInstance();
+        cal1.setTime(tempDateStart);
+        cal2.setTime(tempDateEnd);
+        DateFormat df1 = new SimpleDateFormat("yyyy-MM-dd");
+        DateFormat df2 = new SimpleDateFormat("yyyy-MM-dd");
+
+        //매개변수로 들어온 모드 확인 후 검색기간 수정
+        if (mode.equals("today")) {
+            cal2.add(Calendar.DATE, +1);
+        } else if (mode.equals("yesterday")) {
+            cal1.add(Calendar.DATE, -1);
+            cal2.add(Calendar.DATE, +1);
+        } else if (mode.equals("week")) {
+            cal1.add(Calendar.DATE, -7);
+            cal2.add(Calendar.DATE, +1);
+        } else if (mode.equals("month")) {
+            cal1.add(Calendar.MONTH, -1);
+            cal2.add(Calendar.DATE, +1);
+        }
+
+        String dateStartStr = df1.format(cal1.getTime());
+        String dateEndStr = df2.format(cal2.getTime());
+
+        return findByDate(dateStartStr, dateEndStr, page);
+    }
+
 }
