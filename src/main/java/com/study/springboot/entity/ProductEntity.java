945742587415a6f7da998a68cb403cdad60738a4
psk910903 <104986080+psk910903@users.noreply.github.com>
2023-02-16 14:36:50 +0900

    0216 선교 장바구니, 주문, 상품 entity 통합

--- src/main/java/com/study/springboot/entity/ProductEntity.java
+++ src/main/java/com/study/springboot/entity/ProductEntity.java
@@ -17,39 +17,39 @@ public class ProductEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "item_no", nullable = false)
-    private Long item_no;
+    private Long itemNo;
     @Column(name = "item_category", nullable = false)
-    private String item_category;
+    private String itemCategory;
     @Column(name = "item_name", nullable = false)
-    private String item_name;
+    private String itemName;
     @Column(name = "item_option_color", nullable = true)
-    private String item_option_color;
+    private String itemOptionColor;
     @Column(name = "item_option_size", nullable = true)
-    private String item_option_size;
+    private String itemOptionSize;
     @Column(name = "item_price", nullable = false)
-    private Long item_price;
+    private Long itemPrice;
     @Column(name = "item_image_url", nullable = true)
-    private String item_image_url;
+    private String itemImageUrl;
     @Column(name = "item_info", nullable = true)
-    private String item_info;
+    private String itemInfo;
     @Column(name = "item_info_image_url_1", nullable = true)
-    private String item_info_image_url_1;
+    private String itemInfoImageUrl1;
     @Column(name = "item_info_image_url_2", nullable = true)
-    private String item_info_image_url_2;
+    private String itemInfoImageUrl2;
     @Column(name = "item_update_datetime", nullable = true)
-    private LocalDateTime item_update_datetime = LocalDateTime.now(); // 생성일,수정일
+    private LocalDateTime itemUpdateDatetime = LocalDateTime.now(); // 생성일,수정일
 
     @Builder
-    public ProductEntity(Long item_no, String item_category, String item_name, String item_option_color, String item_option_size, Long item_price, String item_image_url, String item_info, String item_info_image_url_1, String item_info_image_url_2) {
-        this.item_no = item_no;
-        this.item_category = item_category;
-        this.item_name = item_name;
-        this.item_option_color = item_option_color;
-        this.item_option_size = item_option_size;
-        this.item_price = item_price;
-        this.item_image_url = item_image_url;
-        this.item_info = item_info;
-        this.item_info_image_url_1 = item_info_image_url_1;
-        this.item_info_image_url_2 = item_info_image_url_2;
+    public ProductEntity(Long itemNo, String itemCategory, String itemName, String itemOptionColor, String itemOptionSize, Long itemPrice, String itemImageUrl, String itemInfo, String itemInfoImageUrl1, String itemInfoImageUrl2) {
+        this.itemNo = itemNo;
+        this.itemCategory = itemCategory;
+        this.itemName = itemName;
+        this.itemOptionColor = itemOptionColor;
+        this.itemOptionSize = itemOptionSize;
+        this.itemPrice = itemPrice;
+        this.itemImageUrl = itemImageUrl;
+        this.itemInfo = itemInfo;
+        this.itemInfoImageUrl1 = itemInfoImageUrl1;
+        this.itemInfoImageUrl2 = itemInfoImageUrl2;
     }
 }
