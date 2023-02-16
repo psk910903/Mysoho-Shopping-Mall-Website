1e49d8e48b1a244086853d3e4b6d8328b269dede
ejkim230101 <122240608+ejkim230101@users.noreply.github.com>
2023-02-16 13:00:08 +0900

    완료본

--- src/main/java/com/study/springboot/controller/Controller3.java
+++ src/main/java/com/study/springboot/controller/Controller3.java
@@ -6,126 +6,111 @@ import com.study.springboot.entity.review.ReviewEntity;
 import com.study.springboot.repository.ReviewRepository;
 import com.study.springboot.service.ReviewService;
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.data.domain.Page;
-
-
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/admin/review/")
+@RequestMapping("/admin/review")
 public class Controller3 {
     private final ReviewService reviewService;
-
+    private final ReviewRepository reviewRepository;
 
     @RequestMapping("/")
-    public String reviewMain(){
-        return "redirect:/admin/review/listForm";
+    public String main(){
+        return "redirect:/admin/review/list";
     }
 
-//    @RequestMapping("/test")
-//    public String test(){
-//    }
-
-//    원본
-//    @RequestMapping("/listForm")
-//    public String reviewListForm(Model model) {
-//        //정렬해서 검색하면 되는지 보기
-//        List<ReviewResponseDto> list = reviewService.findAll();
-//        model.addAttribute("list", list);
-//        model.addAttribute("listcount", list.size());
-//        return "/admin/review/list";
-//    }
-    //강사님 정렬
-    @RequestMapping("/listForm")
+    //list만 사용하는 전체 출력
+    @RequestMapping("/list")
     public String listForm( Model model,
-                            @RequestParam(value = "page", defaultValue = "0") int page) {
-        //실제론 값이 안 들어와 page=0이 됨. 매개변수로 0에 해당되는 10개의 데이타만 옴
-        Page<ReviewEntity> paging = reviewService.getList(page);
-        model.addAttribute("paging", paging);
-        List<ReviewResponseDto> list = new ArrayList<>();
-        for (ReviewEntity entity : paging) {
-            list.add(new ReviewResponseDto(entity));
+                            @RequestParam(value = "dateStart", required = false) String dateStart,
+                            @RequestParam(value = "dateEnd", required = false) String dateEnd,
+                            @RequestParam(value = "page", defaultValue = "0") int page,
+                            @RequestParam(value = "keyword", required = false ) String keyword,
+                            @RequestParam(value = "findBy", required = false ) String findBy) throws ParseException {
+
+        Page<ReviewResponseDto> list = null;
+        int totalPage;
+        List<Integer> pageList;
+        if ((findBy == null) && (keyword == null) && (dateStart == null) && (dateEnd == null)) {
+            //페이징된 리스트 가져오기
+            list = reviewService.getPageList(page);
+        }else {
+            if((!dateStart.equals("")) && (dateEnd.equals(""))){//dateStart 값만 있을때
+                list = reviewService.findByDate(dateStart, page);
+            }
+            else if((!dateStart.equals("")) && (!dateEnd.equals(""))){//
+                list = reviewService.findByDate(dateStart, dateEnd, page);
+            }else {
+                list = reviewService.findByKeyword(findBy, keyword, page);
+            }
         }
+        //출력페이지 5개로 고정
+        totalPage = list.getTotalPages();
+        pageList = reviewService.getPageList(totalPage, page);
+
         model.addAttribute("list", list);
-        model.addAttribute("listcount", list.size());
-        return "/admin/review/list";
+        model.addAttribute("findBy", findBy);
+        model.addAttribute("keyword", keyword);
+        model.addAttribute("pageList", pageList);
+        //검색 상품 개수
+        long listCount = reviewRepository.count();
+        model.addAttribute("listCount", listCount);
+
+        return "/admin/review/reviewList";
     }
 
-    //Page<ReviewEntity> findByMemberIdContaining(String keyword, Pageable sort);
-    //단건조회
-    @RequestMapping("/modify/{reviewNo}")
-    public String reviewModify(@PathVariable("reviewNo") int reviewNo,
-                         Model model) {
-        ReviewResponseDto review = reviewService.findById((long) reviewNo);
-        model.addAttribute("review", review);
-        return "/admin/review/modify";
+
+    //글쓰기
+    @RequestMapping("write")
+    public String write(){
+        return "/admin/review/writeForm";
     }
+
+    @RequestMapping("writeAction")
+    public String writeAction(ReviewSaveResponseDto dto){
+        reviewService.save(dto);
+        return "redirect:/admin/review/list";
+    }
+
+    //단건조회
+    @RequestMapping("modify")
+    public String modify(@RequestParam("reviewNo") int reviewNo,
+                         Model model) {
+        ReviewEntity review = reviewService.findById((long) reviewNo);
+
+        model.addAttribute("review", review);
+        return "/admin/review/reviewModify";
+    }
+    //단건수정
     @RequestMapping("modifyAction")
     @ResponseBody
-    public String reviewModifyAction(ReviewSaveResponseDto dto,
-                               @RequestParam("reviewNo") int reviewNo
-                               ){
-        ReviewEntity entity = reviewService.update((long)reviewNo,dto);
-        if(entity.getReviewNo() == (long)reviewNo){
-            return "<script>alert('수정 성공');location.href='/admin/review/listForm';</script>";
-        }else {
+    public String modifyAction(ReviewSaveResponseDto reviewSaveResponseDto){
+        boolean result = reviewService.update(reviewSaveResponseDto);
+        if(!result){
             return "<script>alert('수정 실패');history.back();</script>";
-        }
-    }
-//    @RequestMapping("/update")
-//    public String reviewUpdate (@RequestParam("reviewNo") String reviewNo,
-//                          Model model){
-//
-//
-//
-//        List <ReviewResponseDto> list = new ArrayList<>();
-//        String[] arrIdx = reviewNo.split(",");
-//        for (int i=0; i<arrIdx.length; i++) {
-//
-//            System.out.println(arrIdx[i]);
-//            //수정할 목록 찾아옴
-//            Optional<ReviewEntity> optional = reviewService.findById((long)Integer.parseInt(arrIdx[i]));
-//            System.out.println(optional.get());
-//            ReviewResponseDto dto = new ReviewResponseDto(optional.get());
-//            list.add(dto);
-//        }
-////        List<MemberEntity> list = memberRepository.findAll();//엔티티로 전부 출력
-////        model.addAttribute("list",list);
-//        System.out.println("1111");
-//        model.addAttribute("list",list);
-//        model.addAttribute("listcount",list.size());
-//        return "/admin/review/test" ;
-//    }
-
-    @RequestMapping("/deleteAction")
-    @ResponseBody
-    public String reviewDeleteAction (@RequestParam("reviewNo") int reviewNo){
-        boolean result = reviewService.delete((long)reviewNo);
-        System.out.println(result);
-        if(!result) {
-            return "<script>alert('리뷰삭제 실패');history.back();</script>";
-        }
-        return "<script>alert('리뷰삭제 성공');location.href='/admin/review/listForm';</script>";
+        }return "<script>alert('수정 성공');location.href='/admin/review/list';</script>";
     }
 
-    @RequestMapping ("/delete")
-    @ResponseBody
-    public String reviewDelete (@RequestParam("reviewNo") String reviewNo){
-        System.out.println(reviewNo);
-        boolean result = reviewService.deleteAll(reviewNo);
-        if(!result) {
-            return "<script>alert('리뷰삭제 실패');history.back();</script>";
-        }
-        return "<script>alert('글삭제 성공!'); location.href='/admin/review/listForm';</script>";
+    //삭제하기
+    @RequestMapping("/delete")
+    public String delete (@RequestParam("reviewNo") String reviewNo){
+        reviewService.delete(reviewNo);
+        return "redirect:/admin/review/list";
     }
-
-
-
 }//class
