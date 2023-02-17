748eef444666245cddfbe172c31d51828e899e12
ejkim230101 <122240608+ejkim230101@users.noreply.github.com>
2023-02-17 11:29:24 +0900

    Add files via upload
    0217 은진 상품문의

---
+++ src/main/java/com/study/springboot/repository/InReplyRepository.java
@@ -0,0 +1,12 @@
+package com.study.springboot.repository;
+
+import com.study.springboot.entity.inquiry.InReplyEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface InReplyRepository extends JpaRepository<InReplyEntity,Long> {
+    List<InReplyEntity> findAllByReplyInquiryNo (Long replyInquiryNo);
+}
