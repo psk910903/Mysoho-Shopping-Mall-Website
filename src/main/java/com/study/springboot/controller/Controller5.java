f70474a367ef5e9276cb170b8410e79a50ea4023
ejkim230101 <122240608+ejkim230101@users.noreply.github.com>
2023-02-17 11:26:41 +0900

    2-17 은진 상품문의

--- src/main/java/com/study/springboot/controller/Controller5.java
+++ src/main/java/com/study/springboot/controller/Controller5.java
@@ -1,82 +1,129 @@
-//package com.study.springboot.controller;
-//
-//import com.study.springboot.dto.qna.QnaReplySaveDto;
-//import com.study.springboot.dto.qna.QnaResponseDto;
-//import com.study.springboot.dto.qna.QnaSaveDto;
-//import com.study.springboot.service.QnaReplyService;
-//import com.study.springboot.service.QnaService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RequiredArgsConstructor
-//@Controller
-//@RequestMapping("/admin/qna")
-//public class Controller5 {
-//
-//    private final QnaService qnaService;
-//    private final QnaReplyService qnaReplyService;
-//    //
-//    @GetMapping("/QnaContent/{idx}")
-//    public String content(@PathVariable("idx") long idx, Model model){
-//        List<QnaResponseDto> list = qnaService.findbyIdx(idx);
-//        model.addAttribute("list",list);
-//        return "/admin/qna/QnaContent";
-//    }
-//
-//
-//    //
-//    @GetMapping("/QnaList")
-//    public String list(Model model){
-//        List<QnaResponseDto> list = qnaService.findAll();
-//        model.addAttribute("list",list);
-//        return "/admin/qna/QnaList";
-//    }
-//    @GetMapping("/QnaWrite")
-//    public String write(){return "/admin/qna/QnaWrite";}
-//
-//    //글쓰기 버튼을 눌렀을 때,경고창을 띄워주는 페이지
-//    @PostMapping("/QnaWriteDo")
-//    @ResponseBody
-//    public String writedo(QnaSaveDto qnaSaveDto){
-//        boolean result= qnaService.save(qnaSaveDto.toSaveEntity());
-//        if(!result){
-//            return "<script>alert('글쓰기 실패');history.back();</script>";
-//        }
-//        return "<script>alert('글쓰기 성공');location.href='/admin/qna/QnaList';</script>";    }
-//
-//    // 삭제하기
-//    @GetMapping("/delete")
-//    @ResponseBody
-//    public String QnaDelete(Long qna_id) {
-//        boolean result =  qnaService.QnaDelete(qna_id);
-//        if(!result){
-//            return "<script>alert('삭제 실패');history.back();</script>";
-//        }
-//        return "<script>alert('삭제 성공');location.href='/admin/qna/QnaList';</script>";
-//    }
-//
-//    @GetMapping("/QnaContent")
-//    public String QnaContent() {
-//        return "admin/qna/QnaContent";
-//    }
-//
-//    //↓ 댓글(잠시 주석처리)
-//     //댓글달기 버튼을 눌렀을 때, 뜨는 경고창 페이지
-//
-//    @PostMapping("/QnaReplyWriteDo")
-//    @ResponseBody
-//    public String replywritedo(QnaReplySaveDto qnaReplySaveDto){
-//        boolean result= qnaReplyService.save(qnaReplySaveDto.toSaveReplyEntity());
-//        if(!result){
-//            return "<script>alert('댓글쓰기 실패');history.back();</script>";
-//        }
-//        return "<script>alert('댓글쓰기 성공');location.href='/admin/qna/QnaContent';</script>";    }
-//
-//
-//
-//
-//}
+package com.study.springboot.controller;
+
+import com.study.springboot.dto.inquiry.InReplyResponseDto;
+import com.study.springboot.dto.inquiry.InReplySaveResponseDto;
+import com.study.springboot.dto.inquiry.InquiryResponseDto;
+import com.study.springboot.entity.Reply;
+import com.study.springboot.entity.inquiry.InReplyEntity;
+import com.study.springboot.repository.InReplyRepository;
+import com.study.springboot.repository.InquiryRepository;
+import com.study.springboot.service.InReplyService;
+import com.study.springboot.service.InquiryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.ParseException;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/admin/inquiry")
+public class Controller5 {
+    public final InquiryService inquiryService;
+    public final InquiryRepository inquiryRepository;
+    public final InReplyService inReplyService;
+    private final InReplyRepository inReplyRepository;
+
+    @GetMapping ("/")
+    public String inquiryMain(){
+        return "redirect:/admin/inquiry/list";
+    }
+
+    @GetMapping("/list")
+    public String list(Model model,
+                       @RequestParam(value = "dateStart", required = false) String dateStart,
+                       @RequestParam(value = "dateEnd", required = false) String dateEnd,
+                       @RequestParam(value = "keyword", required = false ) String keyword,
+                       @RequestParam(value = "findBy", required = false ) String findBy,
+                       @RequestParam(value = "page", defaultValue = "0") int page) throws ParseException {
+        Page<InquiryResponseDto> list = null;
+        if(keyword == null && findBy == null && dateStart == null && dateEnd == null){
+            list = inquiryService.getPage(page);
+        }else {
+            if(!dateStart.equals("") && dateEnd.equals("")){
+                list = inquiryService.findByDate(dateStart, page);
+            } else if ((!dateStart.equals("")) && (!dateEnd.equals(""))) {
+                list = inquiryService.findByDate(dateStart, dateEnd, page);
+            } else {
+                list = inquiryService.findByKeyword(findBy, keyword, page);
+            }
+        }
+        int totalPage = list.getTotalPages();
+        List<Integer> pageList = inquiryService.getPageList(totalPage, page);
+        model.addAttribute("list", list);
+        model.addAttribute("findBy", findBy);
+        model.addAttribute("keyword", keyword);
+        model.addAttribute("pageList", pageList);
+
+        long listCount = inquiryRepository.count();
+        model.addAttribute("listCount",listCount );
+
+        return "admin/inquiry/list";
+    }
+
+    @RequestMapping ("/content")
+    public String content(@RequestParam("inquiryNo")Long inquiryNo,
+                          //@RequestParam("inquiryNo")Long replyInquiryNo,
+                          Model model){
+
+        InquiryResponseDto dto = inquiryService.findById(inquiryNo);
+        model.addAttribute("dto",dto);
+        System.out.println(inquiryNo);
+        List<InReplyEntity> list = inReplyRepository.findAllByReplyInquiryNo(inquiryNo);
+        //List<InReplyResponseDto> list = inReplyService.findAllByReplyInquiryNo(inquiryNo);
+        model.addAttribute("list",list);
+
+        return "admin/inquiry/content";
+    }
+
+//    리플 달기
+
+    @RequestMapping("/writeAction")
+    @ResponseBody
+    public String replyWrite(InReplySaveResponseDto dto,
+                             @RequestParam("replyInquiryNo") Long replyInquiryNo ){
+        boolean result = inReplyService.save(dto);
+        if(result) {
+            return "<script>alert('댓글쓰기 성공!'); location.href='/admin/inquiry/content?inquiryNo=" + replyInquiryNo + "'; </script>";
+        }else{
+            return "<script>alert('댓글쓰기 실패!'); history.back();</script>";
+        }
+    }
+    @RequestMapping("/modifyAction")
+    @ResponseBody
+    public String modify(InReplySaveResponseDto dto,
+                         @RequestParam("replyNo") Long replyNo,
+                         @RequestParam("replyInquiryNo") Long replyInquiryNo){
+        System.out.println("replyNo = " + replyNo);
+        System.out.println("replyInquiryNo = " + replyInquiryNo);
+        boolean result = inReplyService.modify( dto,replyNo );
+        if(result) {
+            return "<script>alert('댓글수정 성공!'); location.href='/admin/inquiry/content?inquiryNo=" + replyInquiryNo + "'; </script>";
+        }else{
+            return "<script>alert('댓글수정 실패!'); history.back();</script>";
+        }
+    }
+
+    @RequestMapping("/deleteAction")
+    @ResponseBody
+    public String delete(@RequestParam("replyNo") Long replyNo,
+                         @RequestParam("replyInquiryNo") Long replyInquiryNo){
+        boolean result = inReplyService.delete(replyNo);
+        if(result){
+            return "<script>alert('댓글삭제 성공!'); location.href='content?inquiryNo=" + replyInquiryNo + "';</script>";
+        }else {
+            return "<script>alert('댓글삭제 실패!'); history.back();</script>";
+        }
+
+    }
+
+
+
+
+
+
+
+}
