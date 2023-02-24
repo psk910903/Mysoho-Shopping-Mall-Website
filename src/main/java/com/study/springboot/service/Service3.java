94e5d3337b223525c71b0e489ae222e4e294e64b
ejkim230101 <122240608+ejkim230101@users.noreply.github.com>
2023-02-24 10:58:39 +0900

    0224 은진 서비스3 수정

--- src/main/java/com/study/springboot/service/Service3.java
+++ src/main/java/com/study/springboot/service/Service3.java
@@ -1,4 +1,11 @@
 package com.study.springboot.service;
 
+import com.study.springboot.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class Service3 {
+
 }
