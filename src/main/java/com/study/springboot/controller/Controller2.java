8ffc6c39fe6ee5b12dd9c74d28363aed1384f9ea
ejkim230101 <122240608+ejkim230101@users.noreply.github.com>
2023-02-27 16:10:15 +0900

    0227 은진 import문 추가

--- src/main/java/com/study/springboot/controller/Controller2.java
+++ src/main/java/com/study/springboot/controller/Controller2.java
@@ -13,6 +13,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
