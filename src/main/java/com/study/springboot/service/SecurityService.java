65bac0628a9269dcbb151bd88aabc134aa5eb5ec
ejkim230101 <122240608+ejkim230101@users.noreply.github.com>
2023-02-27 15:34:06 +0900

    0227 은진 이름 변경

--- src/main/java/com/study/springboot/service/SecurityService.java
+++ src/main/java/com/study/springboot/service/SecurityService.java
@@ -21,27 +21,6 @@ import java.util.Optional;
 public class SecurityService implements UserDetailsService {
 
     final private MemberRepository memberRepository;
-
-    //username(id)을 통해, 스프링 시큐리티에 사용자 정보(password,role)를 전달한다.
-//    @Override
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        List<GrantedAuthority> authorities = new ArrayList<>();
-//        //USER/ADMIN권한(역할)등을 넣어줌
-//        authorities.add(new SimpleGrantedAuthority(UserRole.USER.getValue()));//권한은 ROLE로 시작됨.
-//        //암호 "1234"문자열을 Bcrypt암호화(bcrypt-generator.com)에서 암호 생성하여 넣는다.//username,password,role반환
-//        return new User("hong","$2a$12$z/2MFyi/C/mGA.w48Z2LzOdmfN2npyEFMLSFlJGRElc3bnE0CIuK6",authorities);
-//    }
-    //db에서 데이타 가져오기
-
-
-//        @Override
-//        public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//            List<GrantedAuthority> authorities = new ArrayList<>();
-//            //ADMIN 권한/역할을 넣는다.
-//            authorities.add(new SimpleGrantedAuthority(UserRole.ADMIN.getValue()));
-//            //“1234”문자열을 Bcrypt 사이트(bcrypt-generator.com)에서 암호 생성하여 넣는다.
-//            return new User("hong", "$2a$12$CLFNXQConBP9WhVNqpWYY.5RmFID66xYzDI8yOFRf.RC/Qac41QjG", authorities);
-//        }
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
@@ -58,7 +37,7 @@ public class SecurityService implements UserDetailsService {
         } else {
             authorities.add(new SimpleGrantedAuthority(UserRole.USER.getValue()));
         }
-        return new User(memberEntity.getMemberId(), memberEntity.getMemberPw(), authorities);
+        return new User(memberEntity.getUsername(), memberEntity.getPassword(), authorities);
     }
 
 }
