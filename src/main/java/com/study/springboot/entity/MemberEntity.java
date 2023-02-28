1ded2013079a226384e3b4a7c251b01d5ebe1be8
ejkim230101 <122240608+ejkim230101@users.noreply.github.com>
2023-02-28 14:53:34 +0900

    0228은진 coupon추가

--- src/main/java/com/study/springboot/entity/MemberEntity.java
+++ src/main/java/com/study/springboot/entity/MemberEntity.java
@@ -31,6 +31,9 @@ public class MemberEntity {
     private String memberPhone; // = member_phone = memberphone
     @Column(name = "member_mileage")
     private Long memberMileage;
+    @Column(name = "member_coupon")
+    private Long memberCoupon;//
+
     @Column(name = "member_addr_number")
     private String memberAddrNumber;
     @Column(name = "member_addr1")
@@ -57,7 +60,7 @@ public class MemberEntity {
                         String memberAddrNumber, String memberAddr1,
                         String memberAddr2, String memberRole,
                         LocalDate memberJoinDatetime, LocalDate memberExitDatetime,
-                        String memberExited) {
+                        String memberExited, Long memberCoupon) {
         this.memberNo = memberNo;
         this.username = username;
         this.password = password;
@@ -66,6 +69,7 @@ public class MemberEntity {
         this.memberEmail = memberEmail;
         this.memberPhone = memberPhone;
         this.memberMileage = memberMileage;
+        this.memberCoupon = memberCoupon;
         this.memberAddrNumber = memberAddrNumber;
         this.memberAddr1 = memberAddr1;
         this.memberAddr2 = memberAddr2;
